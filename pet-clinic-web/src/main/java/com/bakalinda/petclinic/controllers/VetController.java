922b71a3f0ae9480df124e7df2b79d62f7608d4a
Balsa <balsa.bane@gmail.com>
2019-01-30 22:42:25 +0100

    Revert "List all the Vets on Vets Index Page, closes #7"
    This reverts commit 17287a4fe984e43528d40f5c5d723fd3dc1a2b96.

--- pet-clinic-web/src/main/java/com/bakalinda/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/bakalinda/petclinic/controllers/VetController.java
@@ -1,24 +1,14 @@
 package com.bakalinda.petclinic.controllers;
 
 
-import com.bakalinda.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    private final VetService vetService;
-
-    public VetController(VetService vetService) {
-        this.vetService = vetService;
-    }
-
     @RequestMapping({"vets", "/vets/index", "/vets/index.html"})
-
-    public String listVets(Model model){
-        model.addAttribute("vets", vetService.findAll());
+    public String listVets(){
         return "vets/index";
     }
 }
