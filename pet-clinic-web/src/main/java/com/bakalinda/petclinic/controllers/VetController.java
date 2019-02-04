790044be5c341310c426c17afcdb21702d0813aa
Balsa <balsa.bane@gmail.com>
2019-02-04 15:14:41 +0100

    Apply master layout to Vet List Page, closes #15

--- pet-clinic-web/src/main/java/com/bakalinda/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/bakalinda/petclinic/controllers/VetController.java
@@ -1,14 +1,26 @@
 package com.bakalinda.petclinic.controllers;
 
 
+import com.bakalinda.petclinic.services.VetService;
+import com.bakalinda.petclinic.services.map.VetServiceMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 
-    @RequestMapping({"vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    private final VetService vetService;
+
+    public VetController(VetService vetService){
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/index", "/index.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets" , vetService.findAll());
         return "vets/index";
     }
 }
