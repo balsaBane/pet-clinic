a9b4ddc73a98518a1f52ba4cb6977fc28f39ccdd
Balsa <balsa.bane@gmail.com>
2019-02-06 10:15:58 +0100

    Create Speciality Map Service, closes #21

--- pet-clinic-data/src/main/java/com/bakalinda/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/bakalinda/petclinic/model/Visit.java
@@ -3,6 +3,7 @@ package com.bakalinda.petclinic.model;
 import java.time.LocalDate;
 
 public class Visit extends BaseEntity {
+
     private LocalDate date;
     private String description;
     private Pet pet;
---
+++ pet-clinic-data/src/main/java/com/bakalinda/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package com.bakalinda.petclinic.services;
+
+import com.bakalinda.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService <Speciality, Long>{
+
+}
